9bdbe5c2c25b01ab82dd337cc097d34b33fc79fe
Chris Cook <chris.mt.cook@gmail.com>
2017-01-18 13:35:10 +1100

    Get error highlighting on required checkboxes working.

--- template.php
+++ template.php
@@ -40,13 +40,6 @@ function uikit_base_preprocess_field(&$variables) {
 }
 
 /**
- * Implements THEME_preprocess_form_element().
- */
-function uikit_base_preprocess_form_element(&$variables) {
-  $variables['element']['#children'] = str_replace('required error', 'required error invalid', $variables['element']['#children']);
-}
-
-/**
  * Implements THEME_preprocess_node().
  */
 function uikit_base_preprocess_node(&$variables) {
@@ -398,6 +391,80 @@ function uikit_base_form_element_label($variables) {
 
   // The leading whitespace helps visually separate fields from inline labels.
   return ' <label' . drupal_attributes($attributes) . '>' . $t('!title !required', array('!title' => $title, '!required' => $required)) . "</label>\n";
+
+}
+
+/**
+ * Implements THEME_form_element().
+ */
+function uikit_base_form_element($variables) {
+
+  $element = &$variables['element'];
+
+  // This function is invoked as theme wrapper, but the rendered form element
+  // may not necessarily have been processed by form_builder().
+  $element += array(
+    '#title_display' => 'before',
+  );
+
+  // Add element #id for #type 'item'.
+  if (isset($element['#markup']) && !empty($element['#id'])) {
+    $attributes['id'] = $element['#id'];
+  }
+  // Add element's #type and #name as class to aid with JS/CSS selectors.
+  $attributes['class'] = array('form-item');
+  if (!empty($element['#type'])) {
+    $attributes['class'][] = 'form-type-' . strtr($element['#type'], '_', '-');
+  }
+  if (!empty($element['#name'])) {
+    $attributes['class'][] = 'form-item-' . strtr($element['#name'], array(' ' => '-', '_' => '-', '[' => '-', ']' => ''));
+  }
+  // Add a class for disabled elements to facilitate cross-browser styling.
+  if (!empty($element['#attributes']['disabled'])) {
+    $attributes['class'][] = 'form-disabled';
+  }
+  // Add a class to the form wrapper if this element has attracted an error
+  if (strpos($element['#children'], 'error') !== FALSE) {
+    $attributes['class'][] = 'form-error';
+  }
+  $output = '<div' . drupal_attributes($attributes) . '>' . "\n";
+
+  // Add UI Kit error classes to the form element if it has attracted an error
+  $element['#children'] = str_replace('error', 'error invalid', $element['#children']);
+
+  // If #title is not set, we don't display any label or required marker.
+  if (!isset($element['#title'])) {
+    $element['#title_display'] = 'none';
+  }
+  $prefix = isset($element['#field_prefix']) ? '<span class="field-prefix">' . $element['#field_prefix'] . '</span> ' : '';
+  $suffix = isset($element['#field_suffix']) ? ' <span class="field-suffix">' . $element['#field_suffix'] . '</span>' : '';
+
+  switch ($element['#title_display']) {
+    case 'before':
+    case 'invisible':
+      $output .= ' ' . theme('form_element_label', $variables);
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+
+    case 'after':
+      $output .= ' ' . $prefix . $element['#children'] . $suffix;
+      $output .= ' ' . theme('form_element_label', $variables) . "\n";
+      break;
+
+    case 'none':
+    case 'attribute':
+      // Output no label and no required marker, only the children.
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+  }
+
+  if (!empty($element['#description'])) {
+    $output .= '<div class="description">' . $element['#description'] . "</div>\n";
+  }
+
+  $output .= "</div>\n";
+
+  return $output;
 
 }
 
